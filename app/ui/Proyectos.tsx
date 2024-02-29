6e397deed5ab787dbcb87506d5a966d3044f9691
Alex Cordova Huaracha <u19101608@utp.edu.pe>
2024-02-29 15:01:07 -0500

    inicio

---
+++ app/ui/Proyectos.tsx
@@ -0,0 +1,15 @@
+import { AcademicCapIcon } from "@heroicons/react/24/outline";
+
+export default function Proyectos() {
+  return (
+    <div
+      id="proyectos"
+      className="sm:w-[450px] md:w-[600px] lg:w-[900px] xl:w-[900px]"
+    >
+      <div className="flex items-center">
+        <AcademicCapIcon className="w-6 h-6 mr-2" />
+        <h2 className="lg:text-2xl mb-1 text-slate-950 text-xl font-semibold">PROJECTOS</h2>
+      </div>
+    </div>
+  );
+}
